99ed11e0c752fbf2789660b119b545b0e1780745
mihoy3rd <1138745761@qq.com>
2025-03-30 17:32:47 +0800

    clean fs

--- fs/namei.c
+++ fs/namei.c
@@ -121,11 +121,6 @@
 
 #define EMBEDDED_NAME_MAX	(PATH_MAX - offsetof(struct filename, iname))
 
-#ifdef VENDOR_EDIT
-//Jiemin.Zhu@PSW.Android.SdardFs, 2017/12/12, Add for sdcardfs delete dcim record
-#define DCIM_DELETE_ERR  999
-#endif /* VENDOR_EDIT */
-
 struct filename *
 getname_flags(const char __user *filename, int flags, int *empty)
 {
@@ -2594,14 +2589,7 @@ static int may_delete(struct vfsmount *mnt, struct inode *dir, struct dentry *vi
 		return -ENOENT;
 	BUG_ON(!inode);
 
-#ifdef VENDOR_EDIT
-//Jiemin.Zhu@PSW.Android.SdcardFs.1444856, 2018/06/26, Add for avoid crash when dcim proctect
-	if (victim->d_parent->d_inode != dir) {
-		return -EBUSY;
-	}
-#else
 	BUG_ON(victim->d_parent->d_inode != dir);
-#endif /* VENDOR_EDIT */
 	audit_inode_child(dir, victim, AUDIT_TYPE_CHILD_DELETE);
 
 	error = inode_permission2(mnt, dir, MAY_WRITE | MAY_EXEC);
@@ -3829,11 +3817,6 @@ exit3:
 	dput(dentry);
 exit2:
 	mutex_unlock(&path.dentry->d_inode->i_mutex);
-#ifdef VENDOR_EDIT
-//Jiemin.Zhu@PSW.Android.SdardFs, 2017/12/12, Add for sdcardfs delete dcim record
-	if (error == DCIM_DELETE_ERR)
-		error = 0;
-#endif /* VENDOR_EDIT */
 	mnt_drop_write(path.mnt);
 exit1:
 	path_put(&path);
@@ -3964,11 +3947,6 @@ exit2:
 		dput(dentry);
 	}
 	mutex_unlock(&path.dentry->d_inode->i_mutex);
-#ifdef VENDOR_EDIT
-//Jiemin.Zhu@PSW.Android.SdardFs, 2017/12/12, Add for sdcardfs delete dcim record
-	if (error == DCIM_DELETE_ERR)
-		error = 0;
-#endif /* VENDOR_EDIT */
 	if (inode)
 		iput(inode);	/* truncate the inode here */
 	inode = NULL;
@@ -3986,7 +3964,6 @@ exit1:
 		inode = NULL;
 		goto retry;
 	}
-	debug_record_file("delete", pathname, error);
 	return error;
 
 slashes:
@@ -4572,7 +4549,6 @@ exit1:
 		goto retry;
 	}
 exit:
-	debug_record_file("rename", oldname, error);
 	return error;
 }
 
